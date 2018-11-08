8a5347802cf16743851121ec88c6c3ef98c9d88e
t-kusakabe <ttmac0704@gmail.com>
2018-11-08 13:55:29 +0900

    create component

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {SubComponent} from './sub-component';
+
+const App: any = () => (
+  <div>
+    <h1>Hello React!</h1>
+    <SubComponent name='My Counter for TypeScript' />
+  </div>
+);
+
+ReactDOM.render(<App />, document.querySelector('#app'));
